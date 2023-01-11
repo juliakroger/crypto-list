d2f93083105cc671705d9cdcd61b34c4f87958b5
Julia Kroger <juliakroger@gmail.com>
2023-01-11 17:57:16 -0300

    feat: add support to tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "@/components/App";
 import "@/styles/index.scss";
+import "@/styles/index.css";
 
 const container = document.getElementById("root");
 const root = createRoot(container!);
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    container: {
+      center: true,
+    },
+    extend: {
+      colors: {
+        background: "#000000",
+      },
+    },
+  },
+  plugins: [],
+};
