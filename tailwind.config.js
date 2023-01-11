0aca512ba54d279d998cf0491edea0e6efe1cc7f
Julia Kroger <juliakroger@gmail.com>
2023-01-11 18:01:17 -0300

    fix: update current index.scss styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,6 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    container: {
-      center: true,
-    },
     extend: {
       colors: {
         background: "#000000",
