b561fdc0ec51d4f19685ca804cbbc93a255eb3af
Julia Kroger <juliakroger@gmail.com>
2023-01-12 19:41:03 -0300

    fix: migrate spinner to taiwind

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "@/components/App";
-import "@/styles/index.scss";
 import "@/styles/index.css";
 
 const container = document.getElementById("root");
