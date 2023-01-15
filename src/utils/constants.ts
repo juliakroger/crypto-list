02d3e93fb3b7e6093f6be139f520e6d967c1b64e
Julia Kroger <juliakroger@gmail.com>
2023-01-15 10:11:02 -0300

    fix: update query params

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,3 @@
-export const URL = "https://api.coingecko.com/api/v3/coins/markets";
+export const URL = "https://api.coingecko.com/api/v3";
 export const ITEMS_PER_PAGE = 25;
 export const REFRESH_INTERVAL_SECONDS = 60;
--- src/utils/query.ts
+++ src/utils/query.ts
@@ -4,7 +4,10 @@ import { URL, ITEMS_PER_PAGE } from "@/utils/constants";
 export const getMarketCap = async (page = 1, itensPerPage = ITEMS_PER_PAGE) => {
   const [response, error] = await axios
     .get(
-      `${URL}?vs_currency=usd&order=market_cap_desc&per_page=${itensPerPage}&page=${page}&sparkline=true`
+      `${URL}/coins/markets?vs_currency=usd&order=gecko_desc&per_page=${itensPerPage}&page=${page}&sparkline=true`,
+      {
+        "Access-Control-Allow-Origin": "*",
+      }
     )
     .then((res) => [res.data, null])
     .catch((err) => [null, err]);
