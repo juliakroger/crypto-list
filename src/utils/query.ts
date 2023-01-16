fa77713b29de3f92d9e0a4208b5fcef692897df4
Julia Kroger <juliakroger@gmail.com>
2023-01-16 10:10:34 -0300

    fix: update config headers

--- src/utils/query.ts
+++ src/utils/query.ts
@@ -1,13 +1,18 @@
-import axios from "axios";
+import axios, { AxiosRequestConfig } from "axios";
 import { URL, ITEMS_PER_PAGE } from "@/utils/constants";
+
+const config: AxiosRequestConfig = {
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Headers": "*",
+  },
+};
 
 export const getMarketCap = async (page = 1, itensPerPage = ITEMS_PER_PAGE) => {
   const [response, error] = await axios
     .get(
       `${URL}/coins/markets?vs_currency=usd&order=gecko_desc&per_page=${itensPerPage}&page=${page}&sparkline=true`,
-      {
-        "Access-Control-Allow-Origin": "*",
-      }
+      config
     )
     .then((res) => [res.data, null])
     .catch((err) => [null, err]);
