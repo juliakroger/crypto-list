9e99e35dfce13c296cc243b4819b18b0cca2185b
Julia Kroger <juliakroger@gmail.com>
2023-01-23 18:17:59 -0300

    fix: update query to receive params

--- src/utils/query.ts
+++ src/utils/query.ts
@@ -1,6 +1,8 @@
 import axios, { AxiosRequestConfig } from "axios";
 import { URL, ITEMS_PER_PAGE } from "@/utils/constants";
 
+axios.defaults.baseURL = URL;
+
 const config: AxiosRequestConfig = {
   headers: {
     "Access-Control-Allow-Origin": "*",
@@ -8,12 +10,22 @@ const config: AxiosRequestConfig = {
   },
 };
 
-export const getMarketCap = async (page = 1, itensPerPage = ITEMS_PER_PAGE) => {
+export const getMarketCap = async (
+  page = 1,
+  itensPerPage = ITEMS_PER_PAGE,
+  vs_currency = "usd",
+  order = "gecko_desc"
+) => {
+  const params = new URLSearchParams({
+    page: page.toString(),
+    itensPerPage: itensPerPage.toString(),
+    vs_currency,
+    order,
+    sparkline: "true",
+  });
+
   const [response, error] = await axios
-    .get(
-      `${URL}/coins/markets?vs_currency=usd&order=gecko_desc&per_page=${itensPerPage}&page=${page}&sparkline=true`,
-      config
-    )
+    .get("/coins/markets", { ...config, params })
     .then((res) => [res.data, null])
     .catch((err) => [null, err]);
 
